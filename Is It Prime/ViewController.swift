d9f14db54ba394e0fe89e8b1088451e4ece4164d
Will Epperson <wepperson@wills-macbook-pro.local>
2015-09-29 21:36:50 -0400

    Commit #1

--- Is It Prime/ViewController.swift
+++ Is It Prime/ViewController.swift
@@ -10,6 +10,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var responseLabel: UILabel!
+    @IBOutlet var userNumber: UITextField!
+    
+    @IBAction func submit(sender: AnyObject) {
+        if let input = Int(userNumber.text!) {
+            
+            if input <= 0 {
+                responseLabel.text = "\(input) is not a valid number. Please enter a positive value."
+            }
+            else if input == 1 || input == 2 {
+                responseLabel.text = "Yes! \(input) is prime."
+            } else if isPrime(input){
+                responseLabel.text = "Yes! \(input) is prime."
+            } else {
+                responseLabel.text = "No! \(input) is not prime."
+            }
+        } else {
+            responseLabel.text = "Please enter a positive integer."
+        }
+    }
+    
+    // function for prime
+    func isPrime(guess:Int) ->Bool {
+        
+        // prime calculation
+        let MAX = guess
+        var numbers:[Int] = []
+        var primes:[Int] = []
+        
+        for var i = 3; i <= MAX; i=i+2 {
+            numbers.append(i)
+        }
+        
+        let sqrtNum : Int = Int(sqrt(Double(MAX))) + 1
+        
+        while !numbers.isEmpty && numbers[0] <= sqrtNum {
+            
+            let front:Int = numbers.removeFirst()
+            
+            primes.append(front)
+            
+            for var i = 0; i < numbers.count; i++ {
+                if numbers[i] % front == 0 {numbers.removeAtIndex(i)}
+            }
+        }
+        
+        // append the rest of the numbers to the prime list
+        for rest in numbers {
+            primes.append(rest)
+        }
+        
+        if(guess == primes[primes.count - 1] && primes.count > 0) {return true;}
+        else {return false}
+        
+    }
+    
+    // X Code generated
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
